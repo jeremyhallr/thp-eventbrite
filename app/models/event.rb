077fc965d96afa5e736b35392eac97a9d056691f
jeremyhallr <jeremy.h8@hotmail.fr>
2020-05-11 14:02:52 +0200

    Event validations update

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,19 @@ class Event < ApplicationRecord
   belongs_to :admin, class_name: "User"
   has_many :attendances
   has_many :users, through: :attendances
+
+  validates :start_date, presence: true
+  validates :duration,
+    presence: true,
+    numericality: { greater_than: 0 }
+  validates :title,
+    presence: true,
+    length: { in: 5..140 }
+  validates :description,
+    presence: true,
+    length: { in: 20..1000 }
+  validates :price,
+    numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 1000},
+    presence: true
+  validates :location, presence: true
 end
